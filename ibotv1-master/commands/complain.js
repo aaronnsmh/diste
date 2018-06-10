bb3a4c755cdaf037c7a9e42e1abecec9bb28bdbe
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-06-10 20:22:39 +0100

    Update complain.js

--- ibotv1-master/commands/complain.js
+++ ibotv1-master/commands/complain.js
@@ -1,16 +1,18 @@
 exports.run = async (Discord, client, message, args) => {
 let complaint = args.slice(0).join(" ");
+if(!complaint.length < 1) return message.reply("you need to specify your complaint!");
+if(!complaint.length < 3) return message.reply("please increase the size of your complaint.");
 const author = message.author;
 let staffc = message.guild.channels.find("name", "complaints-review")
 const embed = new Discord.RichEmbed()
             .setColor("#4286f4")
             .setTimestamp()
             .setTitle("New Complaint")
-            .setAuthor(message.author.tag, message.author.avatarURL)
+            .setAuthor(message.author, message.author.avatarURL)
             .setFooter("Complaint made at ", client.avatarURL)
             .setURL("https://ibot.space")
             .setThumbnail(client.avatarURL)
-            .setDescription("Description: \n" + complaint);
+            .setDescription(complaint);
 
             const embed2 = new Discord.RichEmbed()
                         .setColor("#4286f4")
